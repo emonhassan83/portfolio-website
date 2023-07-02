c8961fe887ef4a3847164a36ee41b3b520f6df35
Emon <emonhasan7650@gmail.com>
2023-07-02 13:55:16 +0600

    phone responsive done

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -14,7 +14,7 @@ import { Pagination } from "swiper";
 const Testimonial = () => {
     return (
         <section className='testimonial container section' id='testimonial'>
-            <h2 className="section__title">My Client Feedback</h2>
+            <h2 className="section__title">Client Feedback</h2>
             <span className="section__subtitle">Explore Testimonial</span>
             <Swiper className="testimonial__container"
             loop={true}
