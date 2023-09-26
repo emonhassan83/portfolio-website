2dbc6cc06732203601232458abe6c3b42ae3e8f0
Emon <emonhasan7650@gmail.com>
2023-09-27 00:17:20 +0600

    skill and portfolio section updated

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -18,16 +18,16 @@ const Portfolio = () => {
     <section className="portfolio section" id="portfolio">
       <h2 className="section__title">Portfolio</h2>
       <span className="section__subtitle">Most recent work</span>
-
+      {/* 1st Project */}
       <div className="portfolio__container container">
         <div className="portfolio__content grid">
           <img className="portfolio__img" src={img1} alt="" />
           <div className="portfolio__data">
             <h3 className="portfolio__title">Artistry Academia</h3>
             <p className="portfolio__description">
-              Artistry Academia is a summer camp school & that allows students
-              to enroll and learn specific art activities. It focuses on
-              front-end and backend developments.
+              MingleMotion Express is a versatile video editing platform that
+              empowers users to craft a wide range of professional quality
+              videos. It focuses on front-end and backend developments.
             </p>
             <span onClick={() => toggleTab(1)} className="portfolio__button">
               Demo
@@ -59,6 +59,119 @@ const Portfolio = () => {
                     <li className="portfolio__modal-feature">
                       <VscDebugBreakpointLog className="portfolio__modal-icon" />
                       <p className="portfolio__modal-info">
+                        The website has integrated Firebase authentication to
+                        implement private routes and uses JWT for authorization.
+                      </p>
+                    </li>
+                    <li className="portfolio__modal-feature">
+                      <VscDebugBreakpointLog className="portfolio__modal-icon" />
+                      <p className="portfolio__modal-info">
+                        Website main functionalities like video upload, add
+                        audio, image, text, animation, effect, fill, and export
+                        and more.
+                      </p>
+                    </li>
+                    <li className="portfolio__modal-feature">
+                      <VscDebugBreakpointLog className="portfolio__modal-icon" />
+                      <p className="portfolio__modal-info">
+                        Implement dashboard stores data on MongoDB database and
+                        state management using Redux Toolkit.
+                      </p>
+                    </li>
+                    <li className="portfolio__modal-feature">
+                      <VscDebugBreakpointLog className="portfolio__modal-icon" />
+                      <p className="portfolio__modal-info">
+                        Implements stripe and SSL commerce payment system for
+                        pricing and donate page functionality.
+                      </p>
+                    </li>
+                    <li className="portfolio__modal-feature">
+                      <VscDebugBreakpointLog className="portfolio__modal-icon" />
+                      <p className="portfolio__modal-info">
+                        Implement dark-light mode, message option, profile
+                        update, dynamic feedback, and various functionality.
+                      </p>
+                    </li>
+                  </ul>
+                  <p className="portfolio__modal-time">
+                    <span>Created:</span> Aug 2023 - Sep 2023
+                  </p>
+                  <p className="portfolio__modal-technology">
+                    <span>Technologies:</span> React,Typescript, Redux, Firebase
+                    authentication, MongoDB, stripe, ssl-commerz, NodeJS, CSS
+                    frameworks like Tailwind CSS, DaisyUI and etc.
+                  </p>
+                  <p className="portfolio__modal-link">
+                    <span>View -</span>{" "}
+                    <a
+                      href="https://motion-mingle.web.app/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Live Site
+                    </a>{" "}
+                    |{" "}
+                    <a
+                      href="https://github.com/shanto001971/shanto001971-online-video-editing-platform-client"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Client-Side Code
+                    </a>{" "}
+                    |{" "}
+                    <a
+                      href="https://github.com/shanto001971/online-video-editing-platform-server"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Server Side Code
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* 2nd Project */}
+        <div className="portfolio__content grid">
+          <img className="portfolio__img" src={img2} alt="" />
+          <div className="portfolio__data">
+            <h3 className="portfolio__title">Artistry Academia</h3>
+            <p className="portfolio__description">
+              Artistry Academia is a summer camp school & that allows students
+              to enroll and learn specific art activities. It focuses on
+              front-end and backend developments.
+            </p>
+            <span onClick={() => toggleTab(2)} className="portfolio__button">
+              Demo
+              <AiOutlineArrowRight className="portfolio__button-icon" />
+            </span>
+
+            <div
+              className={
+                toggleState === 2
+                  ? "portfolio__modal active-modal"
+                  : "portfolio__modal"
+              }
+            >
+              <div className="portfolio__modal-container">
+                <div className="portfolio__modal-img">
+                  <img className="portfolio__modal-img" src={img2} alt="" />
+                </div>
+
+                <div className="portfolio__modal-content">
+                  <RxCross1
+                    onClick={() => toggleTab(0)}
+                    className="portfolio__modal-close"
+                  />
+
+                  <h3 className="portfolio__modal-title">Artistry Academia</h3>
+                  <p className="portfolio__modal-time"></p>
+                  <h6 className="portfolio__modal-features-title">Features:</h6>
+                  <ul className="portfolio__modal-features">
+                    <li className="portfolio__modal-feature">
+                      <VscDebugBreakpointLog className="portfolio__modal-icon" />
+                      <p className="portfolio__modal-info">
                         Use Tailwind CSS framework for developing a responsive
                         website.
                       </p>
@@ -66,8 +179,8 @@ const Portfolio = () => {
                     <li className="portfolio__modal-feature">
                       <VscDebugBreakpointLog className="portfolio__modal-icon" />
                       <p className="portfolio__modal-info">
-                        Integrated Firebase for two ways sign-in methods with form
-                        validation.
+                        Integrated Firebase for two ways sign-in methods with
+                        form validation.
                       </p>
                     </li>
                     <li className="portfolio__modal-feature">
@@ -94,9 +207,9 @@ const Portfolio = () => {
                     <span>Created:</span> June 2023 - July 2023
                   </p>
                   <p className="portfolio__modal-technology">
-                    <span>Technologies:</span> React Router, Firebase
-                    authentication, MongoDB, stripe, NodeJS, CSS frameworks like
-                    Tailwind CSS, DaisyUI, HTML.
+                    <span>Technologies:</span> React, Firebase authentication,
+                    MongoDB, stripe, NodeJS, CSS frameworks like Tailwind CSS,
+                    DaisyUI and etc.
                   </p>
                   <p className="portfolio__modal-link">
                     <span>View -</span>{" "}
@@ -129,29 +242,31 @@ const Portfolio = () => {
             </div>
           </div>
         </div>
-
+        {/* 3rd Project */}
         <div className="portfolio__content grid">
-          <img className="portfolio__img" src={img2} alt="" />
+          <img className="portfolio__img" src={img3} alt="" />
           <div className="portfolio__data">
             <h3 className="portfolio__title">Toy Town</h3>
             <p className="portfolio__description">
-            ToyTown is a website that sells educational-related toys (like science, language, & engineering) shop market.  It focuses on front-end and backend developments.
+              ToyTown is a website that sells educational-related toys (like
+              science, language, & engineering) shop market. It focuses on
+              front-end and backend developments.
             </p>
-            <span onClick={() => toggleTab(2)} className="portfolio__button">
+            <span onClick={() => toggleTab(3)} className="portfolio__button">
               Demo
               <AiOutlineArrowRight className="portfolio__button-icon" />
             </span>
 
             <div
               className={
-                toggleState === 2
+                toggleState === 3
                   ? "portfolio__modal active-modal"
                   : "portfolio__modal"
               }
             >
               <div className="portfolio__modal-container">
                 <div className="portfolio__modal-img">
-                  <img className="portfolio__modal-img" src={img2} alt="" />
+                  <img className="portfolio__modal-img" src={img3} alt="" />
                 </div>
 
                 <div className="portfolio__modal-content">
@@ -203,9 +318,8 @@ const Portfolio = () => {
                     <span>Created:</span> May 2023 - June 2023
                   </p>
                   <p className="portfolio__modal-technology">
-                    <span>Technologies:</span> React Router, Firebase
-                    authentication, MongoDB, NodeJS, CSS frameworks (Tailwind
-                    CSS, DaisyUI) etc.
+                    <span>Technologies:</span> React, Firebase authentication,
+                    MongoDB, NodeJS, CSS frameworks (Tailwind CSS, DaisyUI) etc.
                   </p>
                   <p className="portfolio__modal-link">
                     <span>View -</span>{" "}
@@ -227,113 +341,6 @@ const Portfolio = () => {
                     |{" "}
                     <a
                       href="https://github.com/emonhassan83/toy-town-server"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Server Side Code
-                    </a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="portfolio__content grid">
-          <img className="portfolio__img" src={img3} alt="" />
-          <div className="portfolio__data">
-            <h3 className="portfolio__title">Tastebite</h3>
-            <p className="portfolio__description">
-            TasteBite is a website that country-specific cuisine and recipe searches. This project focuses on only the front-end.
-            </p>
-            <span onClick={() => toggleTab(3)} className="portfolio__button">
-              Demo
-              <AiOutlineArrowRight className="portfolio__button-icon" />
-            </span>
-
-            <div
-              className={
-                toggleState === 3
-                  ? "portfolio__modal active-modal"
-                  : "portfolio__modal"
-              }
-            >
-              <div className="portfolio__modal-container">
-                <div className="portfolio__modal-img">
-                  <img className="portfolio__modal-img" src={img3} alt="" />
-                </div>
-
-                <div className="portfolio__modal-content">
-                  <RxCross1
-                    onClick={() => toggleTab(0)}
-                    className="portfolio__modal-close"
-                  />
-
-                  <h3 className="portfolio__modal-title">Toy Town</h3>
-                  <p className="portfolio__modal-time"></p>
-                  <h6 className="portfolio__modal-features-title">Features:</h6>
-                  <ul className="portfolio__modal-features">
-                    <li className="portfolio__modal-feature">
-                      <VscDebugBreakpointLog className="portfolio__modal-icon" />
-                      <p className="portfolio__modal-info">
-                        Taste-bite website focuses only the front-end.
-                      </p>
-                    </li>
-                    <li className="portfolio__modal-feature">
-                      <VscDebugBreakpointLog className="portfolio__modal-icon" />
-                      <p className="portfolio__modal-info">
-                        Builds a responsive, accessible, and dynamic web page.
-                      </p>
-                    </li>
-                    <li className="portfolio__modal-feature">
-                      <VscDebugBreakpointLog className="portfolio__modal-icon" />
-                      <p className="portfolio__modal-info">
-                        The website features implement private route ensuring
-                        secure access.
-                      </p>
-                    </li>
-                    <li className="portfolio__modal-feature">
-                      <VscDebugBreakpointLog className="portfolio__modal-icon" />
-                      <p className="portfolio__modal-info">
-                        Implement a Firebase authentication system, using
-                        email-password, Google, and GitHub accounts.
-                      </p>
-                    </li>
-                    <li className="portfolio__modal-feature">
-                      <VscDebugBreakpointLog className="portfolio__modal-icon" />
-                      <p className="portfolio__modal-info">
-                        I develop the user interface.
-                      </p>
-                    </li>
-                  </ul>
-                  <p className="portfolio__modal-time">
-                    <span>Created:</span> April 2023- May 2023
-                  </p>
-                  <p className="portfolio__modal-technology">
-                    <span>Technologies:</span> React Router, Firebase authentication,
-                    utilizes Tailwind CSS, DaisyUi, libraries (React Icons, Lazy
-                    Load, React to PDF, Toastify) etc.
-                  </p>
-                  <p className="portfolio__modal-link">
-                    <span>View -</span>{" "}
-                    <a
-                      href=" https://tastebite-chef-recipe.web.app/"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Live Site
-                    </a>{" "}
-                    |{" "}
-                    <a
-                      href="https://github.com/emonhassan83/taste-bite-client"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Client-Side Code
-                    </a>{" "}
-                    |{" "}
-                    <a
-                      href="https://github.com/emonhassan83/taste-bite-server"
                       target="_blank"
                       rel="noreferrer"
                     >
