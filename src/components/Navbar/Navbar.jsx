a17b77969c9b71b04e5264e1cb2d7b683ed222f2
Emon <emonhasan7650@gmail.com>
2023-10-22 23:58:35 +0600

    dark mode nav issue solve

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ import { useState } from "react";
 import { useTheme } from "../ThemePrvider/ThemeProvider";
 
 const Navbar = () => {
-  const {theme} = useTheme();
+  const { theme } = useTheme(); // use for dark and light themes
   /* Change Background Header */
   window.addEventListener("scroll", function () {
     const header = document.querySelector(".header");
@@ -23,20 +23,47 @@ const Navbar = () => {
   const [activeNav, setActiveNav] = useState("#home");
 
   return (
-    <header className={`header ${theme.mode === 'dark' ? 'dark-bg-color text-gray-100' : 'bg-color'}`}>
+    <header
+      className={`header ${
+        theme.mode === "dark" ? "dark-bg-color text-gray-100" : "bg-color"
+      }`}
+    >
       <nav className="nav container">
-        <a href="index.html" className={`nav__logo ${theme.mode === 'dark' ? 'text-gray-100' : 'text-dark-color'} `}>
+        <a
+          href="index.html"
+          className={`nav__logo ${
+            theme.mode === "dark" ? "text-gray-100" : "text-dark-color"
+          } `}
+        >
           Emon
         </a>
-        <div className={toggle ? "nav__menu show-menu bg-color" : "nav__menu"}>
-          <ul className={`nav__list ${theme.mode === 'dark' ? 'text-gray-100' : 'text-dark-color'}`}>
+        <div
+          className={`${toggle ? "nav__menu show-menu" : "nav__menu"} ${
+            theme.mode === "dark"
+              ? "text-gray-100 hover:text-white dark-bg-color"
+              : "text-dark-color bg-color"
+          }`}
+        >
+          <ul
+            className={`nav__list ${
+              theme.mode === "dark" ? "text-gray-100" : "text-dark-color"
+            }`}
+          >
             <li className="nav__item">
               <a
                 href="#home"
                 onClick={() => setActiveNav("#home")}
-                className={
-                  activeNav === "#home" ? "nav__link active-link" : "nav__link"
+                className={`
+                  ${
+                    activeNav === "#home"
+                      ? "nav__link active-link"
+                      : "nav__link"
+                  } ${
+                  theme.mode === "dark"
+                    ? "text-gray-100 hover:text-white"
+                    : "text-dark-color"
                 }
+                `}
               >
                 <AiOutlineHome className="nav__icon" /> Home
               </a>
@@ -45,9 +72,17 @@ const Navbar = () => {
               <a
                 href="#about"
                 onClick={() => setActiveNav("#about")}
-                className={
-                  activeNav === "#about" ? "nav__link active-link" : "nav__link"
+                className={`
+                  ${
+                    activeNav === "#about"
+                      ? "nav__link active-link"
+                      : "nav__link"
+                  } ${
+                  theme.mode === "dark"
+                    ? "text-gray-100 hover:text-white"
+                    : "text-dark-color"
                 }
+                `}
               >
                 <AiOutlineUser className="nav__icon" /> About
               </a>
@@ -56,9 +91,17 @@ const Navbar = () => {
               <a
                 href="#skills"
                 onClick={() => setActiveNav("#skill")}
-                className={
-                  activeNav === "#skill" ? "nav__link active-link" : "nav__link"
+                className={`
+                  ${
+                    activeNav === "#skill"
+                      ? "nav__link active-link"
+                      : "nav__link"
+                  } ${
+                  theme.mode === "dark"
+                    ? "text-gray-100 hover:text-white"
+                    : "text-dark-color"
                 }
+                `}
               >
                 <GrDocumentText className="nav__icon" /> Skills
               </a>
@@ -67,11 +110,17 @@ const Navbar = () => {
               <a
                 href="#services"
                 onClick={() => setActiveNav("#services")}
-                className={
-                  activeNav === "#services"
-                    ? "nav__link active-link"
-                    : "nav__link"
+                className={`
+                  ${
+                    activeNav === "#services"
+                      ? "nav__link active-link"
+                      : "nav__link"
+                  } ${
+                  theme.mode === "dark"
+                    ? "text-gray-100 hover:text-white"
+                    : "text-dark-color"
                 }
+                `}
               >
                 <MdOutlineHomeRepairService className="nav__icon" /> Services
               </a>
@@ -80,11 +129,17 @@ const Navbar = () => {
               <a
                 href="#portfolio"
                 onClick={() => setActiveNav("#portfolio")}
-                className={
-                  activeNav === "#portfolio"
-                    ? "nav__link active-link"
-                    : "nav__link"
+                className={`
+                  ${
+                    activeNav === "#portfolio"
+                      ? "nav__link active-link"
+                      : "nav__link"
+                  } ${
+                  theme.mode === "dark"
+                    ? "text-gray-100 hover:text-white"
+                    : "text-dark-color"
                 }
+                `}
               >
                 <BsFillImageFill className="nav__icon" /> Portfolio
               </a>
@@ -93,19 +148,39 @@ const Navbar = () => {
               <a
                 href="#contact"
                 onClick={() => setActiveNav("#contact")}
-                className={
-                  activeNav === "#contact"
-                    ? "nav__link active-link"
-                    : "nav__link"
+                className={`
+                  ${
+                    activeNav === "#contact"
+                      ? "nav__link active-link"
+                      : "nav__link"
+                  } ${
+                  theme.mode === "dark"
+                    ? "text-gray-100 hover:text-white"
+                    : "text-dark-color"
                 }
+                `}
               >
                 <BiSolidMessageSquareDetail className="nav__icon" /> Contract
               </a>
             </li>
           </ul>
-          <RxCross1 className="nav__close text-dark-color" onClick={() => setToggle(!toggle)} />
+          <RxCross1
+            className={`nav__close ${
+              theme.mode === "dark"
+                ? "text-gray-100 hover:text-white"
+                : "text-dark-color"
+            }`}
+            onClick={() => setToggle(!toggle)}
+          />
         </div>
-        <div className="nav__toggle text-dark-color" onClick={() => setToggle(!toggle)}>
+        <div
+          className={`nav__toggle ${
+            theme.mode === "dark"
+              ? "text-gray-100 hover:text-white"
+              : "text-dark-color"
+          }`}
+          onClick={() => setToggle(!toggle)}
+        >
           <FaBars />
         </div>
       </nav>
