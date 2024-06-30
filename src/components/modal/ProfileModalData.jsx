bdfdd77d8c252dff82ca07ace3cf3895d8fc37b2
Emon <emonhasan7650@gmail.com>
2024-06-30 19:30:19 +0600

    modal component data added

---
+++ src/components/modal/ProfileModalData.jsx
@@ -0,0 +1,11 @@
+const ProfileModalData = ({project}) => {
+    console.log(project);
+
+    return (
+        <div>
+            this is project modal data
+        </div>
+    );
+};
+
+export default ProfileModalData;
---
+++ src/components/modal/ProjectModal.jsx
@@ -0,0 +1,86 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useRef, useState } from "react";
+import CustomArrowButton from "../../utils/CustomArrowButton";
+
+export default function ProjectModal() {
+  let [isOpen, setIsOpen] = useState(false);
+  const cancelButtonRef = useRef(null);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  return (
+    <>
+      <div className=" flex items-center justify-center">
+        <div onClick={openModal}>
+          <CustomArrowButton props={"Read Bio"} />
+        </div>
+      </div>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-50 overflow-y-auto"
+          initialFocus={cancelButtonRef}
+          onClose={closeModal}
+        >
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-black/25" />
+            </Transition.Child>
+
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="inline-block w-full max-w-lg sm:max-w-xl md:max-w-2xl p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900"
+                >
+                 {/* give modal title here */}
+                </Dialog.Title>
+                {/* Modal Data component */}
+                <h1>Here give us project info</h1>
+                <div className="mt-4">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center px-4 py-2 text-xs btn-sm font-medium text-gray-700 bg-gray-100 border border-transparent rounded-md hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                    onClick={closeModal}
+                    ref={cancelButtonRef}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+}
